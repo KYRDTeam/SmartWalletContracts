2a5c7d63aab432684bdfb33f1fff984299b35cff
ensete <trungngoxuan92@gmail.com>
2021-02-19 14:55:14 +0700

    Add more test for swap + deposit in uniswap and kyber

--- test/helper.js
+++ test/helper.js
@@ -248,6 +248,10 @@ module.exports.assertGreater = function(val1, val2, errorStr) {
     assert(new BN(val1).should.be.a.bignumber.that.is.greaterThan(new BN(val2)), errorStr);
 }
 
+module.exports.assertGreaterOrEqual = function (val1, val2) {
+  assert(new BN(val1).should.be.a.bignumber.that.is.least(new BN(val2)));
+}
+
 module.exports.assertLesser = function(val1, val2, errorStr) {
     assert(new BN(val1).should.be.a.bignumber.that.is.lessThan(new BN(val2)), errorStr);
 }
