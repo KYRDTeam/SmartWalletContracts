153ae4d0f569d94fb59223162afc9bae74b8ce2d
Anton Buenavista <ay.buenavista@gmail.com>
2021-03-08 02:17:01 +0800

    Fix for getAmountGasTokensToBurn() calculation

--- contracts/exchange/SmartWalletSwapImplementation.sol
+++ contracts/exchange/SmartWalletSwapImplementation.sol
@@ -475,7 +475,6 @@ contract SmartWalletSwapImplementation is SmartWalletSwapStorage, ISmartWalletSw
                     dest.safeTransferFrom(msg.sender, address(lendingImpl), destAmount);
                 }
             } else {
-
                 destAmount = swapUniswapInternal(
                     router,
                     srcAmount,
@@ -760,7 +759,7 @@ contract SmartWalletSwapImplementation is SmartWalletSwapStorage, ISmartWalletSw
         uint256 gasAfter = gasleft();
 
         try
-            burnGasHelper.getAmountGasTokensToBurn(gasBefore.sub(gasAfter))
+            burnGasHelper.getAmountGasTokensToBurn(gasBefore.sub(gasAfter).add(msg.data.length))
         returns (uint256 _gasBurns, address _gasToken) {
             numGasBurns = _gasBurns;
             gasToken = IGasToken(_gasToken);
